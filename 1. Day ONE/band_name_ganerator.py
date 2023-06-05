213b3bb2da01d32424320e8f46891beed92aeac2
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-06 00:35:12 +0200

    2023-06-06

---
+++ 1. Day ONE/band_name_ganerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's name of your pet?\n")
+print("You grew up in " + city_name + " and your pet name is " + pet_name)
+print("Your band name could be " + city_name + " " + pet_name)
