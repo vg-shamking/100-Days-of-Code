94d8f0a9412bfba923b30e553f79b9b45d002e6e
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-14 02:20:17 +0200

    2023-06-14

---
+++ 1. Day ONE/string_manipulation.py
@@ -0,0 +1,4 @@
+print("Hello world!\nHello "
+      "world!\nHello world!" + " Volodymyr!")
+print("\n" + "*" * 25 + "\n")
+print("Hello world!\n\"Hello world!\"\nHello world!" + " " + 'V"o"lodymyr!')
