8dae924466b5841e0f3ebbd30ca6bca36f444cb9
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-15 00:21:05 +0200

    2023-06-15

---
+++ 2. Day TWO/f_string.py
@@ -0,0 +1,18 @@
+print(8 / 3)
+print(int(8 / 3))
+print(round(8 / 3, 0))
+print(round(8 / 3, 1))
+print(round(8 / 3, 2))
+print(round(8 / 3, 3))
+print(round(8 / 3, 4))
+print(round(8 / 3, 5))
+print(round(8 / 3, 6))
+print(round(8 / 3, 7))
+a = (8 // 3)
+print(a)
+print(type(a))
+a -= 1
+print(a)
+score = 0
+height = 1
+print(f"Your score is {score} and height is {height}")
