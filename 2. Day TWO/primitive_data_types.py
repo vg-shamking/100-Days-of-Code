c641ab89bd7b4fb1e8846b2d1abf4b7d46ee41c1
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-16 00:12:54 +0200

    2023-06-16

---
+++ 2. Day TWO/primitive_data_types.py
@@ -0,0 +1,17 @@
+# Python Primitive Data Types
+#
+# String
+
+print("Hello"[4])  # string
+print("12345"[4])  # still string
+print("123" + "78")  # again still string
+
+# Integer
+print(123 + 78)  # integer
+print(123_456_789)  # still integer and _ is ignored
+
+# Float
+print(3.14159)  # Float
+
+# Boolean
+print(True)  # or print(False) -> Boolean
