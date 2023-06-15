e7a0663a3573121f3d2acd04719de74349788328
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-16 00:12:44 +0200

    2023-06-16

---
+++ 2. Day TWO/life_2_weeks.py
@@ -0,0 +1,6 @@
+age = int(input("What is your age? "))
+print(f"Your age is {age} years")
+weeks = age * 52
+print(f"Your age in weeks is {weeks}")
+days = weeks * 7
+print(f"Your age in days is {days}")
