ca85b1f55af773fd6e237468a4a2fe153fecd1b5
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-17 00:04:46 +0200

    2023-06-17

---
+++ 2. Day TWO/math_operators.py
@@ -0,0 +1,11 @@
+# Mathematical Operations in Python
+#
+print("PEMDAS")  # parenthesis - exponents - multiplication+division - addition+subtraction
+# print(3 + 5)
+# print(7 - 3)
+# print(3 * 2)
+# print(6 / 3)  # float
+# print(2 ** 3)
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
+print(6 / 2 * (1 + 2))
