24daa6c3e659a3ed048b532261a923df3d02edaa
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-18 01:06:21 +0200

    2023-06-18

---
+++ 3. Day THREE/if_else_elif.py
@@ -0,0 +1,12 @@
+# Control Flow with if / else
+#
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the roller coaster!")
+elif height == 120:
+    print("You are lucky, exactly to ride!!!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+    
