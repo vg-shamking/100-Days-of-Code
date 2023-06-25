e32f08e9f53b91b24278b7854867143b46d9f3b2
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-25 16:39:22 +0200

    2023-06-25

---
+++ 5. Day FIVE/for_loop_range.py
@@ -0,0 +1,12 @@
+# for loop in range
+
+# for number in range(1, 10):  # up-to but not including 10
+#     print(number)
+
+# for number in range(1, 11, 3):  # step is 3
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+    print(total)
