3d3aa4832a1deff4b1c98deca0b810be93b25c66
Mariia Gorbachova <vg.shamking@gmail.com>
2023-06-27 01:14:15 +0200

    2023-06-27

---
+++ 5. Day FIVE/loops.py
@@ -0,0 +1,13 @@
+# for loop
+# for item in list_of_items
+
+fruits = ["Apple",
+          "Peach",
+          "Pear"
+          ]
+
+for fruit in fruits:
+    # print(fruit)  # let's cook
+    print(fruit + " Pie")
+
+print(fruits)
