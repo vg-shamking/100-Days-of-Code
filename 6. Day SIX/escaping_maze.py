a2cc7a7c9123b567a916efb874215b3bacb7eff8
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-01 00:14:38 +0200

    2023-07-01

---
+++ 6. Day SIX/escaping_maze.py
@@ -0,0 +1,36 @@
+# Reeborg's World
+def move():
+    pass
+
+
+def front_is_clear():
+    pass
+
+
+def wall_in_front():
+    pass
+
+
+def at_goal():
+    pass
+
+
+def right_is_clear():
+    pass
+
+
+"""
+while front_is_clear():
+    move()
+
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()        
+"""
