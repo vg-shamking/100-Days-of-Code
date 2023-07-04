a7536005d55011535f1ae1c80941f84ebcc73a5d
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-04 15:08:02 +0200

    2023-07-04

---
+++ 6. Day SIX/identation.py
@@ -0,0 +1,10 @@
+# Indentation is very important
+sky = "clear"
+
+
+def my_function():  # Zero (0) spaces
+    if sky == "clear":  # Four (4) spaces or Tab ->|
+        print("blue")  # Eight (8) spaces or Two (2) Tabs ->|
+
+
+my_function()
