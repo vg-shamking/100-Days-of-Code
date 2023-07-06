ddf42aa472848d60f5d8265c8d36f42cb930ae11
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-06 15:47:20 +0200

    2023-07-06

---
+++ 6. Day SIX/random_heights.py
@@ -0,0 +1,58 @@
+def jump():
+    pass
+
+
+def move():
+    pass
+
+
+def front_is_clear():
+    pass
+
+
+def wall_in_front():
+    pass
+
+
+def at_goal():
+    pass
+
+
+def right_is_clear():
+    pass
+
+
+def wall_on_right():
+    pass
+
+
+def object_here():
+    pass
+
+
+def carries_object():
+    pass
+
+
+def is_facing_north():
+    pass
+
+
+"""
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()    
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+"""
