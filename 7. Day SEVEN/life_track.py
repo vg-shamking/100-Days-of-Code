89b45bb5fdc684ed74b44e1862805c6ded16b59c
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-09 02:27:53 +0200

    2023-07-09

---
+++ 7. Day SEVEN/life_track.py
@@ -0,0 +1,83 @@
+import random
+
+stages = ['''
+ +---+
+ |   |
+ 0   |
+/|\\  |
+/ \\  |
+     |
+========
+''', '''
+ +---+
+ |   |
+ 0   |
+/|\\  |
+/    |
+     |
+========
+''', '''
+ +---+
+ |   |
+ 0   |
+/|\\  |
+     |
+     |
+========
+''', '''
+ +---+
+ |   |
+ 0   |
+/|   |
+     |
+     |
+========
+''', '''
+ +---+
+ |   |
+ 0   |
+/    |
+     |
+     |
+========
+''', '''
+ +---+
+ |   |
+ 0   |
+     |
+     |
+     |
+========
+''']
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+print(f"Pssst... the solution is {chosen_word}.")
+
+lives = 6
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("YOU LOSE")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("YOU WIN")
+
+    print(stages[lives])
