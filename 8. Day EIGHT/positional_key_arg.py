8ff1f240d7a1ebf0e146e451e610dc83fcde17e3
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-16 01:54:24 +0200

    2023-07-16

---
+++ 8. Day EIGHT/positional_key_arg.py
@@ -0,0 +1,10 @@
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"What is it like in {location}?")
+
+
+# greet_with("Angela", "London")
+# greet_with("Nowhere", "Jack Bauer")
+
+# Keyword Argument
+greet_with(location="London", name="Angela")
