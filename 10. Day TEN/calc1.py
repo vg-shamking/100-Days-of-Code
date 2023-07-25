68d8aadcf7323ba4ef1c250aa2fe7ec518b056bb
Mariia Gorbachova <vg.shamking@gmail.com>
2023-07-25 04:27:41 +0200

    2023-07-25

---
+++ 10. Day TEN/calc1.py
@@ -0,0 +1,30 @@
+"""
+What's the first number?
+Pick an operation:
+What's the next number?
+Type 'y' to continue calculating
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
