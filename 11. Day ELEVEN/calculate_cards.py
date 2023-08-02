cb06635e8d964d6509993e173b7e402a2d3cc8af
Mariia Gorbachova <vg.shamking@gmail.com>
2023-08-02 05:22:03 +0200

    2023-08-02

---
+++ 11. Day ELEVEN/calculate_cards.py
@@ -0,0 +1,25 @@
+import random
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+user_card = []
+computer_card = []
+
+for _ in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
+
+
+def calculate_score(cards):
+    # if 11 in cards and 10 in cards and len(cards) == 2:
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
