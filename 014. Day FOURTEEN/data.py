de97b1a5bb88af1769bd9c5455fde531ddff0e9d
Mariia Gorbachova <vg.shamking@gmail.com>
2023-08-17 02:49:08 +0200

    2023-08-17

---
+++ 014. Day FOURTEEN/data.py
@@ -0,0 +1,26 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 364,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    }
+]
