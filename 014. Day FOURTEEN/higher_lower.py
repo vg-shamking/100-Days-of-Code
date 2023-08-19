c1d0360fb41890db85c85c45c56982e93ba60cdd
Mariia Gorbachova <vg.shamking@gmail.com>
2023-08-19 02:48:34 +0200

    2023-08-19

--- 014. Day FOURTEEN/higher_lower.py
+++ 014. Day FOURTEEN/higher_lower.py
@@ -5,3 +5,10 @@ import random
 print(logo)
 print(vs)
 
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_b == account_a:
+    account_b = random.choice(data)
+
+print(account_a)
+print(account_b)
