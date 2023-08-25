d4a87b8533a3700feb6608c484126edede38d501
Mariia Gorbachova <vg.shamking@gmail.com>
2023-08-25 04:44:22 +0200

    2023-08-25

---
+++ 016. Day SIXTEEN/day-16-start.py
@@ -0,0 +1,13 @@
+import turtle
+# from another_module import another_variable
+# print(another_variable)
+
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
