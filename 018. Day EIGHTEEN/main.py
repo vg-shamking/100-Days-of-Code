3c8f0bb1711d42a39621c82f55ada1377a4a668f
Mariia Gorbachova <vg.shamking@gmail.com>
2023-09-08 03:16:28 +0200

    2023-09-08

---
+++ 018. Day EIGHTEEN/main.py
@@ -0,0 +1,28 @@
+import turtle as t
+import random
+
+t.colormode(255)
+tim = t.Turtle()
+tim.hideturtle()
+color_list = [(229, 228, 226), (225, 223, 224), (199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82), (46, 73, 62), (47, 66, 82)]
+
+tim.setheading(225)
+tim.penup()
+tim.forward(250)
+tim.setheading(0)
+tim.speed("fastest")
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.penup()
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
