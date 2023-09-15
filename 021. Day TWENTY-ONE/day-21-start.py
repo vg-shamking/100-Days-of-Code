eb90f2bd941960426b8eb3817061fe50649988b6
Mariia Gorbachova <vg.shamking@gmail.com>
2023-09-16 01:59:40 +0200

    2023-09-16

---
+++ 021. Day TWENTY-ONE/day-21-start.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("Moving in water")
+
+    def breathe(self):
+        super().breathe()
+        print("doing under water")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
