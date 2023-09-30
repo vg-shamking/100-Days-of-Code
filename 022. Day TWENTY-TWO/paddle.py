6d02a09dc5e7ae8dcdf185989f6a0a8c57792c07
Mariia Gorbachova <vg.shamking@gmail.com>
2023-09-30 06:11:25 +0200

    2023-09-30

---
+++ 022. Day TWENTY-TWO/paddle.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(350, 0)
