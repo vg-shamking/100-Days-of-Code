f7fa885998701fc49549670fdc170e7278d68541
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-02 03:32:24 +0200

    2023-10-02

--- 022. Day TWENTY-TWO/main.py
+++ 022. Day TWENTY-TWO/main.py
@@ -10,20 +10,9 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up(), "Up")
+screen.onkey(r_paddle.go_down(), "Down")
 
 gami_is_on = True
 while gami_is_on:
