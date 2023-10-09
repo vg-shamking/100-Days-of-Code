436b78f07dd3d454b43739c9c6454d8f63e2915f
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-09 05:24:02 +0200

    2023-10-09

--- 022. Day TWENTY-TWO/paddle.py
+++ 022. Day TWENTY-TWO/paddle.py
@@ -9,4 +9,12 @@ class Paddle(Turtle):
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
