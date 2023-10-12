4b35b73dce42e94526835d273db31a282a5c81f3
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-12 04:04:11 +0200

    2023-10-12

--- 022. Day TWENTY-TWO/ball.py
+++ 022. Day TWENTY-TWO/ball.py
@@ -8,3 +8,8 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
