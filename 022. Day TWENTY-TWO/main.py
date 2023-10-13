0704245c729af4e8c1ee1dd790ffb107fcf12ffc
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-13 04:54:57 +0200

    2023-10-13

--- 022. Day TWENTY-TWO/main.py
+++ 022. Day TWENTY-TWO/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -21,6 +22,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 gami_is_on = True
 while gami_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
