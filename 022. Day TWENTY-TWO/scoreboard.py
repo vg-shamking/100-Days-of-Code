eee9f7d9dc7d3de0e3eeb58be6d1fe060267f169
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-26 03:18:04 +0200

    2023-10-26

--- 022. Day TWENTY-TWO/scoreboard.py
+++ 022. Day TWENTY-TWO/scoreboard.py
@@ -4,4 +4,8 @@ from turtle import Turtle
 class Scoreboard(Turtle):
 
     def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
         
