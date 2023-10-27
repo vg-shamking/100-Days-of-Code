1387e57413f72040661c883b5cf1d36cb9a09fe3
Mariia Gorbachova <vg.shamking@gmail.com>
2023-10-27 03:32:27 +0200

    2023-10-27

--- 022. Day TWENTY-TWO/main.py
+++ 022. Day TWENTY-TWO/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ r_paddle = Paddle((360, 0))
 l_paddle = Paddle((-360, 0))
 # top_paddle = Paddle((100,100))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 gami_is_on = True
 while gami_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -37,8 +39,10 @@ while gami_is_on:
     # Detect when R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
